d2cb442667f98f3e07983efaa16d986dbc828772
rahmatjafari <r.jafari1994@yahoo.com>
2016-04-03 23:41:45 +0430

    finish project

--- src/similarity/Similarity.java
+++ src/similarity/Similarity.java
@@ -11,8 +11,58 @@ import java.util.Scanner;
 
 public class Similarity {
 
-    public static void main(String[] args) throws IOException {
-        
+    public static double sim(String s1, String s2) {
+    String ll = s1, ss = s2;
+    if (s1.length() < s2.length()) 
+    { 
+      ll = s2; ss = s1;
+    }
+    int l_Length = ll.length();
+    if (l_Length == 0) 
+    { 
+        return 1.0; 
+    }
+    return (l_Length - distance(ll, ss)) / (double) l_Length;
+  }
+    
+    public static int distance(String s1, String s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+    
+    int[] arr = new int[s2.length() + 1];
+    for (int i = 0; i <= s1.length(); i++) 
+    {
+      int l_Value = i;
+      for (int j = 0; j <= s2.length(); j++) 
+      {
+        if (i == 0)
+          arr[j] = j;
+        else 
+        {
+          if (j > 0) 
+          {
+            int n_Value = arr[j - 1];
+            if (s1.charAt(i - 1) != s2.charAt(j - 1))
+              n_Value = Math.min(Math.min(n_Value, l_Value),arr[j]) + 1;
+            arr[j - 1] = l_Value;
+            l_Value = n_Value;
+          }
+        }
+      }
+      if (i > 0)
+        arr[s2.length()] = l_Value;
+    }
+    return arr[s2.length()];
+  }
+    
+    public static void print_sim(String s1, String s2) {
+    System.out.println(String.format(
+      "%.3f similarity:", sim(s1, s2), s1, s2));
+  }
+    
+    
+    public static void main(String[] args) throws IOException 
+    {  
     String s1,s2,str1,str2;
     Scanner scn = new Scanner(System.in);
     System.out.print("Website1:");
@@ -25,7 +75,7 @@ public class Similarity {
     str2 = d2.body().text();
     str1 = str1.replaceAll("\\s*\\babout\\b\\s* | \\s*\\bafter\\b\\s* | \\s*\\bas\\b\\s* | \\s*\\bat\\b\\s* | \\s*\\bbefore\\b\\s* | \\s*\\bbut\\b\\s* | \\s*\\bby\\b\\s* | \\s*\\bfrom\\b\\s* | \\s*\\bin\\b\\s* | \\s*\\bof\\b\\s* | \\s*\\bon\\b\\s* | \\s*\\bto\\b\\s* | \\s*\\bam\\b\\s* | \\s*\\bis\\b\\s* | \\s*\\bare\\b\\s* | \\s*\\bwas\\b\\s* | \\s*\\bwere\\b\\s* | \\s*\\bdo\\b\\s* | \\s*\\does\\b\\s* | \\s*\\ba\\b\\s* | \\s*\\bin\\b\\s*", " ");
     str2 = str2.replaceAll("\\s*\\babout\\b\\s* | \\s*\\bafter\\b\\s* | \\s*\\bas\\b\\s* | \\s*\\bat\\b\\s* | \\s*\\bbefore\\b\\s* | \\s*\\bbut\\b\\s* | \\s*\\bby\\b\\s* | \\s*\\bfrom\\b\\s* | \\s*\\bin\\b\\s* | \\s*\\bof\\b\\s* | \\s*\\bon\\b\\s* | \\s*\\bto\\b\\s* | \\s*\\bam\\b\\s* | \\s*\\bis\\b\\s* | \\s*\\bare\\b\\s* | \\s*\\bwas\\b\\s* | \\s*\\bwere\\b\\s* | \\s*\\bdo\\b\\s* | \\s*\\does\\b\\s* | \\s*\\ba\\b\\s* | \\s*\\bin\\b\\s*", " ");
-    System.out.println(str1 + "\n" + str2);
+    print_sim(str1,str2);
     }
     
 }
